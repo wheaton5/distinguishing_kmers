ab1f014b8190491fd379d8f3eef702bdce7a7fd7
wheaton5 <whheaton@gmail.com>
2019-01-29 12:35:25 +0000

    now support fasta and fastq gzipped or not

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ extern crate bloom;
 extern crate debruijn;
 
 use flate2::read::GzDecoder;
+use std::io::BufReader;
 use std::io;
 use std::io::prelude::*;
 use std::fs::File;
@@ -58,11 +59,16 @@ fn main() {
                 .long("difference_threshold")
                 .takes_value(true)
                 .help("threshold on difference between counts of infile and subtract files"))
-        //.arg(Arg::with_name("estimated_kmers")
-        //        .long("estimated_kmers")
-        //        .help("estimated number of unique kmers. Good rule of thumb is genome size * 2")
-        //        .takes_value(true)
-        //        .required(true))
+        .arg(Arg::with_name("modimizer")
+                .long("modimizer")
+                .takes_value(true)
+                .required(false)
+                .help("number by which to mod kmer hashes and keep kmer_hash % modimizer == mod_index kmers. Default is 9. Increasing will decrease memory required."))
+        .arg(Arg::with_name("mod_index")
+                .long("mod_index")
+                .takes_value(true)
+                .required(false)
+                .help("remainder after kmer hash mod to keep kmers. Reminder kmer_hash % modimizer == mod_index kmers will be kept. Input -1 to automatically run over all mod_indexes <not implemented>."))
         .get_matches();
     let kmer_size = matches.value_of("kmer_size").unwrap_or("21");
     let kmer_size: usize = kmer_size.to_string().parse::<usize>().unwrap();
@@ -76,8 +82,10 @@ fn main() {
     let min_subtract_count: u16 = min_subtract_count.to_string().parse::<u16>().unwrap();
     let difference_threshold = matches.value_of("difference_threshold").unwrap_or("0");
     let difference_threshold: u16 = difference_threshold.to_string().parse::<u16>().unwrap();
-    //let estimated_kmers = matches.value_of("estimated_kmers").unwrap_or("6000000000");
-    //let estimated_kmers: u32 = estimated_kmers.to_string().parse::<u32>().unwrap();
+    let modimizer = matches.value_of("modimizer").unwrap_or("9");
+    let modimizer: i64 = modimizer.to_string().parse::<i64>().unwrap();
+    let mod_index = matches.value_of("mod_index").unwrap_or("0");
+    let mod_index: i64 = mod_index.to_string().parse::<i64>().unwrap();
     let kmers_in: Vec<_> = matches.values_of("kmers_in").unwrap().collect();
     let kmers_subtract: Vec<_> = matches.values_of("kmers_subtract").unwrap().collect();
     eprintln!("");
@@ -91,40 +99,29 @@ fn main() {
     }
     eprintln!("with at least {} count",min_subtract_count);
     eprintln!("Only considers kmers where the 2bit representation of the kmer % 9 == 0 to improve speed and memory usage. This is reasonable because otherwise for every difference you get {} overlapping {}mers but with this you get on average just over 2 overlapping {}mers.",kmer_size,kmer_size,kmer_size);
-    //eprintln!("Reminder! This program uses counting bloom filters and thus counts are approximate but can only be correct or over-counts");
-    let k_size: usize = 21; // THIS CANNOT BE CHANGED WITHOUT CHANGING Kmer datatype
-    let source_counting_bits = 6;
-    let subtract_counting_bits = 4;
-    //let bloom_kmer_in_counts = count_kmers_fastq(&kmers_in, source_counting_bits, estimated_kmers, k_size);
-    //let bloom_kmer_subtract_counts = count_kmers_fastq(&kmers_subtract, subtract_counting_bits, estimated_kmers, k_size);
-    let kmer_in_counts = count_kmers_fastq_exact(&kmers_in, k_size);
-    let kmer_subtract_counts = count_kmers_fastq_exact(&kmers_subtract, k_size);
-    subtract_kmers_exact(kmer_in_counts, kmer_subtract_counts, min_source_count, min_subtract_count, difference_threshold, k_size);
-    
-    //subtract_kmers(kmers_in, bloom_kmer_in_counts, bloom_kmer_subtract_counts, min_source_count, min_subtract_count, difference_threshold, k_size, estimated_kmers);
+    //let source_counting_bits = 6;
+    //let subtract_counting_bits = 4;
+    let kmer_in_counts = count_kmers_fastq_exact(&kmers_in, modimizer, mod_index);
+    let kmer_subtract_counts = count_kmers_fastq_exact(&kmers_subtract, modimizer, mod_index);
+    subtract_kmers_exact(kmer_in_counts, kmer_subtract_counts, min_source_count, min_subtract_count, difference_threshold);
 }
 
 
 fn subtract_kmers_exact(kmers_in: HashMap<u64,u16>, subtract_counts: HashMap<u64,u16>, 
-                min_source_count: u16, min_subtract_count: u16, difference_threshold: u16, k_size: usize) {
+                min_source_count: u16, min_subtract_count: u16, difference_threshold: u16) {
     for (kmeru64, source_count) in &kmers_in {
         if *source_count > min_source_count {
             let subtract_count = match subtract_counts.get(&kmeru64) {Some(x) => *x, None => 0,};
             if subtract_count <= min_subtract_count && source_count - subtract_count >= difference_threshold {
-                let kmer = Kmer21::from_u64(*kmeru64);
+                let kmer = KmerX::from_u64(*kmeru64);
                 println!("{}\t{}\t{}",kmer.to_string(), source_count, subtract_count);
             }
         }
     }
 }
 
-fn count_kmers_fastq_exact(kmers_in: &Vec<&str>, k_size: usize) -> HashMap<u64,u16> {
+fn count_kmers_fastq_exact(kmers_in: &Vec<&str>, modimizer: i64, mod_index: i64) -> HashMap<u64,u16> {
     let mut kmer_counts: HashMap<u64, u16> = HashMap::new();
-
-    //let mut binary = match File::create("kmers.bin") {
-    //        Err(_) => panic!("couldn't open file for writing"),
-    //        Ok(file) => file,
-    //};
 
     for kmer_file in kmers_in {
         let file = match File::open(kmer_file) {
@@ -133,30 +130,45 @@ fn count_kmers_fastq_exact(kmers_in: &Vec<&str>, k_size: usize) -> HashMap<u64,u
                 panic!("There was a problem opening the file {} with error {}", kmer_file, error)
             },
         };
-        let gz = GzDecoder::new(file);
-        for (line_number, line) in io::BufReader::new(gz).lines().enumerate() {
-            if line_number % 4 == 1 {
-                //let dna: DnaString = DnaString::from_dna_string(&line.unwrap()); //Vec<u64> 2bit encoded
-                //for kmer_start in 0..(dna.len() - k_size + 1) {
-                //    let k: Kmer21 = dna.get_kmer(kmer_start); //statically typed kmer size
-                for k in Kmer21::kmers_from_ascii(&line.unwrap().as_bytes()) {
+        let filetype = kmer_file.split(".").collect::<Vec<&str>>();
+        let filetype = filetype[filetype.len()-1];
+        println!("{}",filetype);
+        let reader: Box<Read> = match filetype { 
+            "gz" => Box::new(GzDecoder::new(file)), 
+            _ => Box::new(file), 
+        }; 
+        let reader = BufReader::new(reader);
+        let mut fasta = false;
+        let mut seq_next = false;
+        
+        for line in reader.lines() {
+            let line = line.unwrap();
+            if line.starts_with("@") {
+                seq_next = true;
+                continue;
+            } else if line.starts_with(">") {
+                seq_next = true;
+                fasta = true;
+                continue;
+            }
+            if seq_next {
+                for k in KmerX::kmers_from_ascii(&line.as_bytes()) {
                     let to_hash = min(k.to_u64(), k.rc().to_u64());
-                    //println!("{}",k.to_string());
-                    if to_hash % 9 == 0 {
-                        //binary.write_all(to_hash);
+                    if to_hash % modimizer as u64 == mod_index as u64 {
                         let mut count = kmer_counts.entry(to_hash).or_insert(0);
                         if *count < 65535 {
                             *count += 1;
                         }
                     }
-                    //kmer_counts.insert(&min(k.to_u64(), k.rc().to_u64()));
                 }
+                if !fasta { seq_next = false; }
             }
         }
     }
     kmer_counts
 }
 
+#[allow(dead_code)]
 fn subtract_kmers(kmers_in: Vec<&str>, kmer_counts: CountingBloomFilter, subtract_counts: CountingBloomFilter, 
                 min_source_count: u32, min_subtract_count: u32, difference_threshold: u32, k_size: usize, estimated_kmers: u32) {
     let mut visited_kmer = BloomFilter::with_rate(0.03, estimated_kmers);
@@ -172,7 +184,7 @@ fn subtract_kmers(kmers_in: Vec<&str>, kmer_counts: CountingBloomFilter, subtrac
             if line_number % 4 == 1 {
                 let dna: DnaString = DnaString::from_dna_string(&line.unwrap());
                 for kmer_start in 0..(dna.len() - k_size + 1) {
-                    let k: Kmer21 = dna.get_kmer(kmer_start);
+                    let k: KmerX = dna.get_kmer(kmer_start);
                     let krc = k.rc();
                     let to_hash = min(k.to_u64(), krc.to_u64());
                     if visited_kmer.contains(&to_hash) {
@@ -196,7 +208,9 @@ fn subtract_kmers(kmers_in: Vec<&str>, kmer_counts: CountingBloomFilter, subtrac
     }
 }
 
-fn count_kmers_fastq(kmers_in: &Vec<&str>, counting_bits: usize, estimated_kmers: u32, k_size: usize) -> CountingBloomFilter {
+#[allow(dead_code)]
+fn count_kmers_fastq(kmers_in: &Vec<&str>, counting_bits: usize, estimated_kmers: u32, k_size: usize, 
+        _modimizer: i64, _mod_index: i64) -> CountingBloomFilter {
     let mut kmer_counts: CountingBloomFilter = CountingBloomFilter::with_rate(counting_bits, 0.05, estimated_kmers);
     for kmer_file in kmers_in {
         let file = match File::open(kmer_file) {
@@ -210,7 +224,7 @@ fn count_kmers_fastq(kmers_in: &Vec<&str>, counting_bits: usize, estimated_kmers
             if line_number % 4 == 1 {
                 let dna: DnaString = DnaString::from_dna_string(&line.unwrap()); //Vec<u64> 2bit encoded
                 for kmer_start in 0..(dna.len() - k_size + 1) {
-                    let k: Kmer21 = dna.get_kmer(kmer_start); //statically typed kmer size
+                    let k: KmerX = dna.get_kmer(kmer_start); //statically typed kmer size
                     println!("{}",k.to_string());
                     kmer_counts.insert(&min(k.to_u64(), k.rc().to_u64()));
                 }
@@ -220,11 +234,11 @@ fn count_kmers_fastq(kmers_in: &Vec<&str>, counting_bits: usize, estimated_kmers
     kmer_counts
 }
 
-type Kmer21 = VarIntKmer<u64, K21>;
+type KmerX = VarIntKmer<u64, KX>;
 #[derive(Debug, Hash, Copy, Clone, Ord, PartialOrd, Eq, PartialEq)]
-pub struct K21;
+pub struct KX;
 
-impl KmerSize for K21 {
+impl KmerSize for KX {
     fn K() -> usize {
         unsafe {
             KMER_SIZE
